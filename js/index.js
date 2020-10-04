e2c274a82011c8d539332cae38eef3eb4da7db09
AkanWealth <akan.asanga@gmail.com>
2020-10-04 16:11:30 -0700

    still working on it

--- js/index.js
+++ js/index.js
@@ -22,7 +22,29 @@ window.addEventListener('load', function(event) {
             loaderHolder.style.display = 'none';
             setTimeout(function name(params) {
                 navbar.style.opacity = '1';
-            }, 1000);
-        }, 2000);
+            }, 500);
+        }, 1000);
     }
 });
+
+const items = document.querySelectorAll(".skill-list li");
+const isInViewport = el => {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+const run = () =>
+    items.forEach(item => {
+        if (isInViewport(item)) {
+            item.classList.add('show');
+        }
+    });
+
+//Events
+window.addEventListener('load', run);
+window.addEventListener('resize', run);
+window.addEventListener('scroll', run);
